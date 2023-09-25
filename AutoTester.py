0ae5947982a94c8ab08babaeb354e382bdaa36d5
Jhonatha de Sodre <108944889+desodre@users.noreply.github.com>
2023-09-24 20:45:21 -0400

    Update AutoTester.py
    um requisitador de informações do dispositivo android para finalizades de QA, usando AT command e ADB

--- AutoTester.py
+++ AutoTester.py
@@ -1,16 +1,23 @@
+import subprocess as sb
 import serial.tools.list_ports
 import sys
 
-class Devices:
+class Devices():
     def __init__(self, portaCom):
         self.portaCom = portaCom
+        getinfo = self.sendAT("AT+DEVCONINFO")
+        self.SN = getinfo[getinfo.index("SN")+3:getinfo.index("SN")+14]
+        self.IMEI = getinfo[getinfo.index("IMEI")+5:getinfo.index("IMEI")+20]
+        self.Model = getinfo[getinfo.index("MN")+3:getinfo.index("MN")+11]
 
-    def sendAT(self, command):
+
+    def sendAT(self, command: str):
         port = serial.Serial(self.portaCom,timeout=1)
         port.write(f"{command}\r\n".encode())
         resposta = port.read(1024)
         port.close()
-        return resposta
+        return resposta.decode()
+
 
     def adbShell(self): pass
 
@@ -19,12 +26,18 @@ class Conection:
     def __init__(self):
         self.__portas  = [port.device for port in serial.tools.list_ports.comports()]
 
+
     @property
     def ports(self):
         return self.__portas
 
-
-
 conec = Conection()
+md = []
 for comP in conec.ports:
-    print(Devices(comP).sendAT("ATI1"))
+    test = Devices(comP)
+    md.append(test)
+
+for obj in md:
+    print(f"Serial: {obj.SN}\nIMEI:{obj.IMEI}\nModel:{obj.Model}")
+
+#test[test.index("MN")+3:test.index("MN")+11]
